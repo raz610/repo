f99ee86ddd77e71bc7ed1a10e0fff85f6b502f7e
raz610 <151939455+raz610@users.noreply.github.com>
2024-12-30 21:34:08 +0200

    Update add.ps1

--- add.ps1
+++ add.ps1
@@ -4,7 +4,6 @@ $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 try {
     # Check if the group exists
     if (-not (Get-LocalGroup -Name $Group -ErrorAction SilentlyContinue)) {
-        Write-Host "The group '$Group' does not exist. Creating it now..."
         New-LocalGroup -Name $Group -Verbose -ErrorAction Stop
     }
 
@@ -15,14 +14,11 @@ try {
     $IsMember = Get-LocalGroupMember -Group $Group -Member $CurrentUser -ErrorAction Stop
 
     if ($IsMember) {
-        Write-Host "The current user '$CurrentUser' was successfully added to the group '$Group'."
         Start-Sleep -Seconds 5
         exit 0  # Success
     } else {
-        Write-Host "Failed to add the current user '$CurrentUser' to the group '$Group'."
         exit 1  # User not in the group
     }
 } catch {
-    Write-Host "An error occurred: $($_.Exception.Message)"
     exit 99  # Unexpected error
 }
