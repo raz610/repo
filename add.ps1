cbc955edc8ffe69ef88720bc1a38c253b7901549
raz610 <151939455+raz610@users.noreply.github.com>
2024-12-30 21:35:37 +0200

    Update add.ps1

--- add.ps1
+++ add.ps1
@@ -1,17 +1,16 @@
-﻿$Group = 'docker-users'
 $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 
 try {
     # Check if the group exists
-    if (-not (Get-LocalGroup -Name $Group -ErrorAction SilentlyContinue)) {
-        New-LocalGroup -Name $Group -Verbose -ErrorAction Stop
+    if (-not (Get-LocalGroup -Name 'docker-users' -ErrorAction SilentlyContinue)) {
+        New-LocalGroup -Name 'docker-users' -Verbose -ErrorAction Stop
     }
 
     # Add the user to the group
-    Add-LocalGroupMember -Group $Group -Member $CurrentUser -Verbose -ErrorAction Stop
+    Add-LocalGroupMember -Group 'docker-users' -Member $CurrentUser -Verbose -ErrorAction Stop
 
     # Check if the user is a member of the group
-    $IsMember = Get-LocalGroupMember -Group $Group -Member $CurrentUser -ErrorAction Stop
+    $IsMember = Get-LocalGroupMember -Group 'docker-users' -Member $CurrentUser -ErrorAction Stop
 
     if ($IsMember) {
         Start-Sleep -Seconds 5
